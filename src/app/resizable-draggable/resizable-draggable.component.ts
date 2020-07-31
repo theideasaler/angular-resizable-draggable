74d7954ec24494392311b0d877acd03f58cd8996
NeoLiu <theideasaler@gmail.com>
2020-07-31 18:51:39 +1000

    update move logics

--- src/app/resizable-draggable/resizable-draggable.component.ts
+++ src/app/resizable-draggable/resizable-draggable.component.ts
@@ -17,10 +17,10 @@ export class ResizableDraggableComponent implements OnInit, AfterViewInit {
   @Input('left') public left: number;
   @Input('top') public top: number;
   @ViewChild("box") public box: ElementRef;
-  private boxSpec: { left: number, top: number, width: number, height: number };
+  private absolutePos: { left: number, top: number };
   public mouse: {x: number, y: number}
   public status: Status = Status.OFF;
-  private mouseAndBoxDetails: {x: number, y: number, left: number, top: number}
+  private mouseClick: {x: number, y: number, left: number, top: number}
 
   ngOnInit() {}
 
@@ -29,13 +29,14 @@ export class ResizableDraggableComponent implements OnInit, AfterViewInit {
   }
 
   private loadBox(){
-    const {left, top, width, height} = this.box.nativeElement.getBoundingClientRect();
-    this.boxSpec = {left, top, width, height};
+    const {left, top} = this.box.nativeElement.getBoundingClientRect();
+    this.absolutePos = {left, top};
   }
 
   setStatus(event: MouseEvent, status: number){
     if(status === 1) event.stopPropagation();
-    else if(status === 2) this.mouseAndBoxDetails = { x: event.clientX, y: event.clientY, left: this.left, top: this.top};
+    else if(status === 2) this.mouseClick = { x: event.clientX, y: event.clientY, left: this.left, top: this.top };
+    else this.loadBox();
     this.status = status;
   }
 
@@ -48,12 +49,12 @@ export class ResizableDraggableComponent implements OnInit, AfterViewInit {
   }
 
   private resize(){
-    this.width = this.mouse.x - this.boxSpec.left;
-    this.height = this.mouse.y - this.boxSpec.top;
+    this.width = this.mouse.x - this.absolutePos.left;
+    this.height = this.mouse.y - this.absolutePos.top;
   }
 
   private move(){
-    this.left = this.mouseAndBoxDetails.left + (this.mouse.x - this.mouseAndBoxDetails.x);
-    this.top = this.mouseAndBoxDetails.top + (this.mouse.y - this.mouseAndBoxDetails.y);
+    this.left = this.mouseClick.left + (this.mouse.x - this.mouseClick.x);
+    this.top = this.mouseClick.top + (this.mouse.y - this.mouseClick.y);
   }
 }
